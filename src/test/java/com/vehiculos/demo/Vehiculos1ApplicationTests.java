5f556fa549b443bf124cdfeec47b18687d6e2adc
yeison hernandez <yeison.hernandez@grupodigital.co>
2021-03-10 11:59:34 -0500

    Creacion de proyecto Spring boot para prueba tecica

---
+++ src/test/java/com/vehiculos/demo/Vehiculos1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehiculos.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vehiculos1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
